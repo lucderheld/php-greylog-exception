d15ab346f726ae48488ccf32c8af2589c6ab7ab9
luc <luc@def-shop.com>
2016-11-12 21:16:14 +0100

    fixed configuration bug

--- src/GreyLogException/GreyLogException.php
+++ src/GreyLogException/GreyLogException.php
@@ -15,11 +15,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-namespace lucderheld\greylogexception;
+namespace GreyLogException;
 /**
  * This is the main class for the GreyLogException module.
  * @author luc <luc@def-shop.com>
- * @version 0.1
+ * @version 0.2
  */
 class GreyLogException extends \Exception {
     
