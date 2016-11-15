3845df487b37d394cf6e16f3e5ba745a4b93db11
luc <luc@def-shop.com>
2016-11-15 21:15:43 +0100

    added first unit test

---
+++ tests/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * Copyright (C) 2016 lsp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$autoloadFile = __DIR__ . '/../vendor/autoload.php';
+if (!file_exists($autoloadFile)) {
+    die("Autoloader cannot be found. " .
+        "Please install dependencies first ('composer install --dev')\n");
+}
+require_once $autoloadFile;
+require_once __DIR__ . '/GreyLogException/TestCase.php';
